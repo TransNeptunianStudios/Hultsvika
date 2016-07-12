57bcacfdcda7db79a5dc73def43d85183e6a287c
Reicher <robin.o.reicher@gmail.com>
2016-07-12 16:08:54 +0200

    initial setup

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,9 +1,10 @@
 import Phaser from 'phaser'
+import Isometric from '../libs/phaser-plugin-isometric'
 import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#EDEEC9'
+    this.stage.backgroundColor = '#4DBD33'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
@@ -21,6 +22,24 @@ export default class extends Phaser.State {
 
     this.load.image('loaderBg', './assets/images/loader-bg.png')
     this.load.image('loaderBar', './assets/images/loader-bar.png')
+
+    this.load.image('cube', './assets/images/cube.png')
+
+    this.game.time.advancedTiming = true;
+
+    // Add and enable the plug-in.
+    this.game.plugins.add(new Phaser.Plugin.Isometric(this.game));
+    // In order to have the camera move, we need to increase the size of our world bounds.
+    this.game.world.setBounds(0, 0, 2048, 1024);
+
+    // Start the IsoArcade physics system.
+    this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+
+    // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
+    // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
+    // When using camera following, it's best to keep the Y anchor set to 0, which will let the camera
+    // cover the full size of your world bounds.
+    this.game.iso.anchor.setTo(0.5, 0);
   }
 
   render () {
--- src/states/Game.js
+++ src/states/Game.js
@@ -3,32 +3,102 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 import {setResponsiveWidth} from '../utils'
 
+var isoGroup, player;
+
 export default class extends Phaser.State {
   init () {}
   preload () {}
 
   create () {
-    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Phaser + ES6 + Webpack')
-    banner.font = 'Nunito'
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.anchor.setTo(0.5)
+    // Create a group for our tiles, so we can use Group.sort
+    isoGroup = this.game.add.group();
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.game.world.centerX,
-      y: this.game.world.centerY,
-      asset: 'mushroom'
-    })
+    // Set the global gravity for IsoArcade.
+    this.game.physics.isoArcade.gravity.setTo(0, 0, -500);
 
-    // set the sprite width to 30% of the game width
-    setResponsiveWidth(this.mushroom, 30, this.game.world)
-    this.game.add.existing(this.mushroom)
+    // Let's make a load of cubes on a grid, but do it back-to-front so they get added out of order.
+    var cube;
+    for (var xx = 1024; xx > 0; xx -= 140) {
+        for (var yy = 1024; yy > 0; yy -= 140) {
+
+            // Create a cube using the new game.add.isoSprite factory method at the specified position.
+            // The last parameter is the group you want to add it to (just like game.add.sprite)
+            cube = this.game.add.isoSprite(xx, yy, 0, 'cube', 0, isoGroup);
+            cube.anchor.set(0.5);
+
+            // Enable the physics body on this cube.
+            this.game.physics.isoArcade.enable(cube);
+
+            // Collide with the world bounds so it doesn't go falling forever or fly off the screen!
+            cube.body.collideWorldBounds = true;
+
+            // Add a full bounce on the x and y axes, and a bit on the z axis.
+            cube.body.bounce.set(1, 1, 0.2);
+
+            // Add some X and Y drag to make cubes slow down after being pushed.
+            cube.body.drag.set(100, 100, 0);
+        }
+    }
+
+    // Create another cube as our 'player', and set it up just like the cubes above.
+    player = this.game.add.isoSprite(128, 128, 0, 'cube', 0, isoGroup);
+    player.tint = 0x86bfda;
+    player.anchor.set(0.5);
+    this.game.physics.isoArcade.enable(player);
+    player.body.collideWorldBounds = true;
+
+    // Set up our controls.
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+    this.game.input.keyboard.addKeyCapture([
+        Phaser.Keyboard.LEFT,
+        Phaser.Keyboard.RIGHT,
+        Phaser.Keyboard.UP,
+        Phaser.Keyboard.DOWN,
+        Phaser.Keyboard.SPACEBAR
+    ]);
+    var space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    space.onDown.add(function () {
+        player.body.velocity.z = 300;
+    }, this);
+
+    // Make the camera follow the player.
+    this.game.camera.follow(player);
+  }
+
+  update () {
+    // Move the player at this speed.
+    var speed = 100;
+
+    if (this.cursors.up.isDown) {
+        player.body.velocity.y = -speed;
+    }
+    else if (this.cursors.down.isDown) {
+        player.body.velocity.y = speed;
+    }
+    else {
+        player.body.velocity.y = 0;
+    }
+
+    if (this.cursors.left.isDown) {
+        player.body.velocity.x = -speed;
+    }
+    else if (this.cursors.right.isDown) {
+        player.body.velocity.x = speed;
+    }
+    else {
+        player.body.velocity.x = 0;
+    }
+
+    // Our collision and sorting code again.
+    this.game.physics.isoArcade.collide(isoGroup);
+    this.game.iso.topologicalSort(isoGroup);
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      //this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }
